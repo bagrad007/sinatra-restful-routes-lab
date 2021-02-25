16968c1f667054c28cd59e509b20a39d307e6a9c
Luke Gill <bagrad007@gmail.com>
2021-02-25 13:55:27 -0500

    commit tree b292274becceb9a8b70d9ce2e8006f5a4afdc094

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,48 @@
 class ApplicationController < Sinatra::Base
   configure do
-    set :public_folder, 'public'
-    set :views, 'app/views'
+    set :public_folder, "public"
+    set :views, "app/views"
   end
 
-  # code actions here!
+  get "/recipes" do
+    @recipes = Recipe.all
+    # binding.pry
+    erb :index
+  end
 
+  get "/recipes/new" do
+    erb :new
+  end
+
+  post "/recipes" do
+    @recipes = Recipe.create(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
+    redirect to "/recipes/#{@recipes.id}"
+  end
+
+  get "/recipes/:id" do
+    # binding.pry
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :show
+  end
+
+  get "/recipes/:id/edit" do
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :edit
+  end
+
+  patch "/recipes/:id" do
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.name = parmas[:name]
+    @recipe.ingredients = parmas[:ingredients]
+    @recipe.cook_time = parmas[:cook_time]
+    @recipe.save
+    redirect to "/recipes/#{@recipes.id}"
+  end
+
+  delete "/recipes/:id" do
+    # binding.pry
+    recipe = Recipe.find_by_id(params[:id])
+    recipe.delete
+    redirect to "/recipes"
+  end
 end
